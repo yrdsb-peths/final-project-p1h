ac952167b79e15ca07816570e351c47f24b430ff
Edison Lim <93136751+335743100@users.noreply.github.com>
2022-01-20 09:08:01 -0500

    Updated Endscreen
    Have to fix background cover
    Need background music

--- EndScreen.java
+++ EndScreen.java
@@ -13,7 +13,8 @@ public class EndScreen extends World
     public static final int ENDING_HEIGHT = GameWorld.WORLD_HEIGHT;
     
     //declaring background variables
-    private GreenfootImage bgImage;
+    private GreenfootImage bg;
+    private GreenfootImage bgImage = new GreenfootImage("EndScreenBackground.png");
     public static final Font ENDING_TITLE_FONT = MainMenu.MENU_TITLE_FONT;
     public static final Color ENDING_TITLE_COLOR = MainMenu.MENU_TITLE_COLOR;
     private String title = "You Died";
@@ -21,30 +22,43 @@ public class EndScreen extends World
     //declaring actors
     private Button backButton;
     
+    //declaring instance variables
+    private int currHeight = ENDING_HEIGHT;
+    
     public EndScreen()
     {    
         // Create a new world with ENDING_WIDTH*ENDING_HEIGHT cells with a cell size of 1x1 pixels.
         super(ENDING_WIDTH, ENDING_HEIGHT, 1);
         
         //drawing background
-        bgImage = new GreenfootImage(getWidth() + 1, getHeight() + 1);
-        bgImage.setColor(Color.WHITE);
-        bgImage.fill();
-        bgImage.setColor(ENDING_TITLE_COLOR);
-        bgImage.setFont(ENDING_TITLE_FONT);
-        bgImage.drawString(title, (getWidth() - (int)(title.length() * ENDING_TITLE_FONT.getSize() * 0.58)) / 2, getHeight() / 2);
-        setBackground(bgImage);
+        bg = new GreenfootImage(getWidth() + 1, getHeight() + 1);
+        bgImage.scale(getWidth() + 1, getHeight() + 1);
+        drawBackground(currHeight);
         
+        //add cover
+        //BackgroundCover cover = new BackgroundCover();
+        //addObject(cover, 0, 0);
         //add button
         backButton = new Button("Back");
         addObject(backButton, getWidth() * 3 / 4, getHeight() * 5 / 6);
     }
     
     public void act(){
+        drawBackground(currHeight);
+        if(currHeight > 0) currHeight--;
+        
         //takes the user back to the main menu
         if(Greenfoot.mouseClicked(backButton)){
             Greenfoot.setWorld(new MainMenu());
             backButton.playClickSound();
         }
+    }
+    
+    private void drawBackground(int coverHeight){
+        bg.drawImage(bgImage, 0, 0);
+        bgImage.setColor(ENDING_TITLE_COLOR);
+        bgImage.setFont(ENDING_TITLE_FONT);
+        bgImage.drawString(title, (getWidth() - (int)(title.length() * ENDING_TITLE_FONT.getSize() * 0.58)) / 2, getHeight() / 2);
+        setBackground(bgImage);
     }
 }
